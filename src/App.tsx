1052b2ed9265910c975e408e566bf1f0afa37410
benjamin.baesu <bbaesu@gmail.com>
2023-03-05 16:36:55 -0800

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Testing
+      <div style={{width: '100%', backgroundColor: 'red'}}>Testing</div>
+      <div style={{width: '100%', flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Testing 2</div>
     </div>
   );
 }
 
 export default App;
+ 
